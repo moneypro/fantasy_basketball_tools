f4449f322b407f9dd9b32d682565c162ed408cff
Hong Cheng <hcheng17@illinois.edu>
2025-10-09 19:05:45 -0700

    Bump year, remove recipient

--- test_utils/create_league.py
+++ test_utils/create_league.py
@@ -6,7 +6,7 @@ from common.io import find_credential_folder, get_single_line_string_from_file, 
 def create_league() -> League:
     return League(
         league_id=int(get_file_content_from_crendential_folder("league_id.txt")),
-        year=2023,
+        year=2025,
         espn_s2=get_file_content_from_crendential_folder("espn_s2.secret"),
         swid=get_file_content_from_crendential_folder("swid.secret")
     )
