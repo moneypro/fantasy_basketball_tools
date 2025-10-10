9fd70e490ee62797ade39d87bf5de1079634592c
Hong Cheng <hcheng17@illinois.edu>
2025-10-09 19:09:36 -0700

    Fix tests, update year

--- test_utils/create_league.py
+++ test_utils/create_league.py
@@ -3,10 +3,10 @@ from espn_api.basketball import League
 from common.io import find_credential_folder, get_single_line_string_from_file, get_file_content_from_crendential_folder
 
 
-def create_league() -> League:
+def create_league(year:int=2026) -> League:
     return League(
         league_id=int(get_file_content_from_crendential_folder("league_id.txt")),
-        year=2025,
+        year=year,
         espn_s2=get_file_content_from_crendential_folder("espn_s2.secret"),
         swid=get_file_content_from_crendential_folder("swid.secret")
     )
